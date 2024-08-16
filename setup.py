cbf2bf2a0b6712c2cbf6eedbe87da57f712459eb
by:abdullah <90639434+abdullah5151@users.noreply.github.com>
2024-08-16 11:49:27 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     description="Textmaker Api",
     long_description=open('README.md', encoding='utf-8').read(),
     long_description_content_type='text/markdown',
-    url="https://github.com/abdullah5151/textmaker",
+    url="https://github.com/abdullah5151/textmakeras",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
