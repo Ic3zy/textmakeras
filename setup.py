0ef401fbc10fe2f8053bd758360665ee97de9b1e
by:abdullah <90639434+abdullah5151@users.noreply.github.com>
2024-08-16 11:48:46 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="textmaker",
-    version="0.1.3",
+    version="0.1.3-beta",
     packages=find_packages(),
     install_requires=[
         "selenium",
